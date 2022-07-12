f18d9ded7fec49b55f09258a0ba2adedd0c944d1
Min-ji99 <61926751+Min-ji99@users.noreply.github.com>
2022-07-12 21:48:14 +0900

    [Gold IV] Title: 주사위 굴리기, Time: 76 ms, Memory: 30840 KB -BaekjoonHub

--- 백준/Gold/14499. 주사위 굴리기/주사위 굴리기.py
+++ 백준/Gold/14499. 주사위 굴리기/주사위 굴리기.py
@@ -1,43 +1,39 @@
-n, m, x, y, k = map(int, input().split())
+import sys
 
-board = []
-dx = [0, 0, -1, 1]
-dy = [1, -1, 0, 0]
-dice = [0, 0, 0, 0, 0, 0]
+input=sys.stdin.readline
+n, m, x, y, k=map(int, input().split())
 
-def turn(dir):
-    a, b, c, d, e, f = dice[0], dice[1], dice[2], dice[3], dice[4], dice[5]
-    if dir == 1: #동
-        dice[0], dice[1], dice[2], dice[3], dice[4], dice[5] = d, b, a, f, e, c
+dx=[0, 0, -1, 1]
+dy=[1, -1, 0, 0]
+dice=[0, 0, 0, 0, 0, 0, 0]
+maps=[]
+for _ in range(n) :
+    maps.append(list(map(int, input().split())))
+    
+moves=list(map(int, input().split()))
 
-    elif dir == 2: #서
-        dice[0], dice[1], dice[2], dice[3], dice[4], dice[5] = c, b, f, a, e, d
+def turn_dice(direction):
+    if direction == 1:
+        dice[1], dice[3], dice[4], dice[6] = dice[3], dice[6], dice[1], dice[4]
+    elif direction == 2:
+        dice[1], dice[3], dice[4], dice[6] = dice[4], dice[1], dice[6], dice[3]
+    elif direction == 3:
+        dice[1], dice[2], dice[5], dice[6] = dice[2], dice[6], dice[1], dice[5]
+    elif direction == 4:
+        dice[1], dice[2], dice[5], dice[6] = dice[5], dice[1], dice[6], dice[2]
 
-    elif dir == 3: #북
-        dice[0], dice[1], dice[2], dice[3], dice[4], dice[5] = e, a, c, d, f, b
-
-    else:
-        dice[0], dice[1], dice[2], dice[3], dice[4], dice[5] = b, f, c, d, a, e
-
-for i in range(n):
-    board.append(list(map(int, input().split())))
-
-comm = list(map(int, input().split()))
-
-nx, ny = x, y
-for i in comm:
-    nx += dx[i-1]
-    ny += dy[i-1]
-
-    if nx < 0 or nx >= n or ny < 0 or ny >= m:
-        nx -= dx[i-1]
-        ny -= dy[i-1]
+for move in moves :
+    x, y=x+dx[move-1], y+dy[move-1]
+    
+    if x<0 or x>=n or y<0 or y>=m :
+        x, y=x-dx[move-1], y-dy[move-1]
         continue
-    turn(i)
-    if board[nx][ny] == 0:
-        board[nx][ny] = dice[-1]
+    
+    turn_dice(move)
+    if maps[x][y]==0 :
+        maps[x][y]=dice[1]
     else:
-        dice[-1] = board[nx][ny]
-        board[nx][ny] = 0
-
-    print(dice[0])
+        dice[1]=maps[x][y]
+        maps[x][y]=0
+        
+    print(dice[6])
