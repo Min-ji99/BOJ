5cc39332a09df3f82330a38ddb39d47119c61bca
Min-ji99 <61926751+Min-ji99@users.noreply.github.com>
2022-07-25 23:51:39 +0900

    [Silver IV] Title: 가로수, Time: 3840 ms, Memory: 40664 KB -BaekjoonHub

---
+++ 백준/Silver/2485. 가로수/가로수.py
@@ -0,0 +1,15 @@
+from math import gcd
+
+n=int(input())
+trees=[int(input()) for _ in range(n)]
+distances=[]
+
+for i in range(1, n) :
+    distances.append(abs(trees[i]-trees[i-1])) #나무들 사이 거리
+
+tmp=distances[0]   
+for  distance in distances: #최대공약수 찾기
+    tmp=gcd(tmp, distance)
+
+answer=(max(trees)-min(trees))//tmp+1 #최대공약수를 기준으
+print(answer-len(trees))
