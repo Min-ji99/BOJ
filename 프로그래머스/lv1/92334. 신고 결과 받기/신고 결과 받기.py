7ff8f9254826315fe6183272385532003565b23b
김민지 <61926751+Min-ji99@users.noreply.github.com>
2022-09-18 14:36:09 +0900

    Update 신고 결과 받기.py

--- 프로그래머스/lv1/92334. 신고 결과 받기/신고 결과 받기.py
+++ 프로그래머스/lv1/92334. 신고 결과 받기/신고 결과 받기.py
@@ -3,16 +3,14 @@ from collections import defaultdict
 def solution(id_list, report, k):
     answer = [0]*len(id_list)
     
-    report_dict={id:[] for id in id_list}
+    report_dict=defaultdict(set)
     
     for r in report :
         r=r.split()
-        report_dict[r[1]].append(r[0])
-    
+        report_dict[r[1]].add(r[0])
+     
     for key, value in report_dict.items() :
-        value=set(value)
-        if len(value)>=k :
+        if len(value)>=k:
             for v in value:
                 answer[id_list.index(v)]+=1
-                
     return answer
