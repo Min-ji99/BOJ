043eb96c871bc66bf6bb01fd3ecc44023b79e384
김민지 <61926751+Min-ji99@users.noreply.github.com>
2022-09-22 12:56:49 +0900

    [Gold II] Title: 꼬인 전깃줄, Time: 132 ms, Memory: 43688 KB -BaekjoonHub

--- 백준/Gold/1365. 꼬인 전깃줄/꼬인 전깃줄.py
+++ 백준/Gold/1365. 꼬인 전깃줄/꼬인 전깃줄.py
@@ -1,15 +1,5 @@
 import sys
-
-# 해당 숫자 이상의 수 중 가장 가까운 인덱스를 리턴하는 함수 ( 정렬이 되어있을 때만 가능 )
-def lower_bound(s, e, v):
-    while s < e:
-        m = (s + e) // 2
-        if res[m] < v:
-            s = m + 1
-        else:
-            e = m
-    return e
-
+from bisect import bisect_left
 
 n = int(sys.stdin.readline())
 line = list(map(int, sys.stdin.readline().split()))
@@ -20,8 +10,8 @@ for i in range(n):
         res.append(line[0])
     if res[-1] < line[i]:   # line[i]가 res의 마지막 요소보다 크면 마지막에 추가
         res.append(line[i])
-    else:   # line[i]가 res의 마지막 요소보다 작으면 lower bound 하여 나온 index위치의 값과 교환
-        tmp = lower_bound(0, len(res), line[i])
+    else:   # line[i]가 res의 마지막 요소보다 작으면 이분탐색 하여 나온 index위치의 값과 교환
+        tmp = bisect_left(res, line[i])
         res[tmp] = line[i]
 
 
