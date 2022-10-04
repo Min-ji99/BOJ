8dfa45d24e56679ba7788ef3fbddf759e266ab75
김민지 <61926751+Min-ji99@users.noreply.github.com>
2022-10-04 11:29:28 +0900

    [Silver I] Title: 회의실 배정, Time: 308 ms, Memory: 57760 KB -BaekjoonHub

--- 백준/Silver/1931. 회의실 배정/회의실 배정.py
+++ 백준/Silver/1931. 회의실 배정/회의실 배정.py
@@ -1,17 +1,13 @@
 import sys
+input=sys.stdin.readline
+n=int(input())
 
-n = int(input())
-timetable = [[0]*2 for i in range(n)]
-for i in range(n) :
-  s, f = map(int, sys.stdin.readline().split())
-  timetable[i][0]=s
-  timetable[i][1]=f
-
-timetable.sort(key=lambda x:(x[1], x[0]))
-cnt = 1
-end_time = timetable[0][1]
-for i in range(1, n) :
-  if timetable[i][0] >= end_time :
-    cnt+=1
-    end_time=timetable[i][1]
-print(cnt)
+timetables=[list(map(int, input().split())) for _ in range(n)]
+timetables.sort(key=lambda x:(x[1], x[0]))
+ans=1
+end_time=timetables[0][1]
+for i in range(1, len(timetables)) :
+    if timetables[i][0]>=end_time:
+        ans+=1
+        end_time=timetables[i][1]
+print(ans)
