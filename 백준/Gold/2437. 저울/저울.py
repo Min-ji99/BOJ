81416ed605ba10dfa87139532faf9ec129f36a0d
김민지 <61926751+Min-ji99@users.noreply.github.com>
2022-10-29 15:31:38 +0900

    [Gold II] Title: 저울, Time: 72 ms, Memory: 30840 KB -BaekjoonHub

---
+++ 백준/Gold/2437. 저울/저울.py
@@ -0,0 +1,16 @@
+'''
+누적합
+'''
+import sys
+
+input=sys.stdin.readline
+n=int(input())
+weights=list(map(int, input().split()))
+weights.sort()
+
+target=1
+for weight in weights:
+    if target<weight:
+        break
+    target+=weight
+print(target)
