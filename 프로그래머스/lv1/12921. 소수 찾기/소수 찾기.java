5cecbc881eb75046a8d6c508cbfac0bd7f8cf3b9
김민지 <61926751+Min-ji99@users.noreply.github.com>
2022-11-02 09:38:58 +0900

    [level 1] Title: 소수 찾기, Time: 14.13 ms, Memory: 53.3 MB -BaekjoonHub

--- 프로그래머스/lv1/12921. 소수 찾기/소수 찾기.java
+++ 프로그래머스/lv1/12921. 소수 찾기/소수 찾기.java
@@ -1,12 +1,12 @@
 class Solution {
     public int solution(int num){
         boolean prime[]=new boolean[num+1];
-        prime[0]=prime[1]=true;
+        prime[0]=prime[1]=true; //소수가 아니면 true
         for(int i=2; i*i<=num; i++){
             if(!prime[i]){
-            	// prime[j] 소수가 아닌 표시
-            	for(int j=i*i; j<=num; j+=i) prime[j] = true;                
-            }  
+                // prime[j] 소수가 아닌 표시
+                for(int j=i*2; j<=num; j+=i) prime[j] = true;
+            }
         }
         int count=0;
         for(int i=2; i<=num; i++){
