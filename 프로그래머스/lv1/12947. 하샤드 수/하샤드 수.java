aa8baabd5303e59ea307901a86dada041a3085e4
김민지 <61926751+Min-ji99@users.noreply.github.com>
2022-11-07 09:18:30 +0900

    [level 1] Title: 하샤드 수, Time: 0.02 ms, Memory: 77.7 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/12947. 하샤드 수/하샤드 수.java
@@ -0,0 +1,22 @@
+class Solution {
+        public int sumOfDigits(int x){
+        int sum=0;
+        while(x>0){
+            sum+=x%10;
+            x/=10;
+        }
+        return sum;
+    }
+    public boolean checkRemainder(int x, int sum){
+        if(x%sum==0){
+            return true;
+        }
+        return false;
+    }
+    public boolean solution(int x) {
+        boolean answer = true;
+        int sum=sumOfDigits(x);
+        answer=checkRemainder(x, sum);
+        return answer;
+    }
+}
