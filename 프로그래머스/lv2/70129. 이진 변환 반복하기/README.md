784e62c9c8b406fc9690ea6fa3c16a95a1c60c0b
김민지 <61926751+Min-ji99@users.noreply.github.com>
2022-11-11 10:01:56 +0900

    [level 2] Title: 이진 변환 반복하기, Time: 10.49 ms, Memory: 82.9 MB -BaekjoonHub

--- 프로그래머스/lv2/70129. 이진 변환 반복하기/README.md
+++ 프로그래머스/lv2/70129. 이진 변환 반복하기/README.md
@@ -1,10 +1,10 @@
 # [level 2] 이진 변환 반복하기 - 70129 
 
-[문제 링크](https://programmers.co.kr/learn/courses/30/lessons/70129) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/70129?language=java) 
 
 ### 성능 요약
 
-메모리: 10.5 MB, 시간: 1.32 ms
+메모리: 82.9 MB, 시간: 10.49 ms
 
 ### 구분
 
---
+++ 프로그래머스/lv2/70129. 이진 변환 반복하기/이진 변환 반복하기.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int[] solution(String s) {
+        int[] answer = new int[2];
+        int cnt=0;
+        int zeroCnt=0;
+        while(s.length()!=1){
+            zeroCnt+=s.length()-s.replace("0", "").length();
+            s=s.replace("0", "");
+            s=Integer.toBinaryString(s.length());
+            cnt+=1;
+        }
+        answer[0]=cnt;
+        answer[1]=zeroCnt;
+        return answer;
+    }
+}
