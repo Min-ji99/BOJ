56cc7488080683f00d87f90b6d565192c2ffed4a
김민지 <61926751+Min-ji99@users.noreply.github.com>
2022-11-18 09:34:00 +0900

    [level 1] Title: 시저 암호, Time: 31.93 ms, Memory: 105 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/12926. 시저 암호/시저 암호.java
@@ -0,0 +1,27 @@
+class Solution {
+    public String solution(String s, int n) {
+        String answer = "";
+        for(int i=0; i<s.length(); i++) {
+            char ch = s.charAt(i);
+            if (ch == ' ') {
+                answer += s.charAt(i);
+                continue;
+            }
+            if (ch >= 'a' && ch <= 'z') {
+                if (ch + n > 'z') {
+                    answer += (char) (ch + n - 26);
+                }else {
+                    answer+=(char)(ch+n);
+                }
+            }else if(ch>='A' && ch<='Z'){
+                if(ch+n>'Z'){
+                    answer+=(char)(ch+n-26);
+                }else{
+                    answer+=(char)(ch+n);
+                }
+            }
+        }
+
+        return answer;
+    }
+}
