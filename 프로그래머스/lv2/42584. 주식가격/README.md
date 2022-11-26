3b07f84ea714e7f08d7053391a9a40710171bb25
김민지 <61926751+Min-ji99@users.noreply.github.com>
2022-11-26 17:55:24 +0900

    [level 2] Title: 주식가격, Time: 93.55 ms, Memory: 16.9 MB -BaekjoonHub

--- 프로그래머스/lv2/42584. 주식가격/README.md
+++ 프로그래머스/lv2/42584. 주식가격/README.md
@@ -1,10 +1,10 @@
 # [level 2] 주식가격 - 42584 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42584) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42584?language=python3) 
 
 ### 성능 요약
 
-메모리: 16.8 MB, 시간: 35.11 ms
+메모리: 16.9 MB, 시간: 93.55 ms
 
 ### 구분
 
--- 프로그래머스/lv2/42584. 주식가격/주식가격.py
+++ 프로그래머스/lv2/42584. 주식가격/주식가격.py
@@ -1,24 +1,5 @@
-# 주식 가격
-from collections import deque
-
-def solution(prices):
-    answer = []
-    queue = deque(prices)
-    # print(queue)
-    
-    while queue:
-        price = queue.popleft()
-        time = 0
-        for num in queue:
-            time += 1
-            if price > num:
-                break
-        answer.append(time)
-    # print(answer)
-    return answer
-'''
-def solution(prices):
-    answer = [0]*len(prices)
+def solution(prices) :
+    answer=[0]*len(prices) 
     for i in range(len(prices)) :
         for j in range(i+1, len(prices)) :
             if prices[i]<=prices[j] :
@@ -27,4 +8,3 @@ def solution(prices):
                 answer[i]+=1
                 break
     return answer
-    '''
