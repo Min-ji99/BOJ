4df527cf25b9a2640a0f4002ce17670a1a2a320e
김민지 <61926751+Min-ji99@users.noreply.github.com>
2022-12-01 10:29:01 +0900

    [unrated] Title: 기사단원의 무기, Time: 0.33 ms, Memory: 78.1 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/136798. 기사단원의 무기/기사단원의 무기.java
@@ -0,0 +1,26 @@
+class Solution {
+    public int divisorCnt(int num){
+        int cnt=0;
+        for(int i=1; i*i<=num; i++){
+            if(i*i==num) {
+                cnt++;
+                continue;
+            }
+            if(num%i==0) {
+                cnt+=2;
+            }
+        }
+        return cnt;
+    }
+    public int solution(int number, int limit, int power) {
+        int answer = 0;
+        
+        for(int i=1; i<=number; i++){
+            int attack=divisorCnt(i);
+            if(attack>limit) answer+=power;
+            else answer+=attack;
+        }
+        
+        return answer;
+    }
+}
