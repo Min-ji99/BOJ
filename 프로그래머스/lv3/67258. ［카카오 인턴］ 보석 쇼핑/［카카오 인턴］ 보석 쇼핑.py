8d302166f4732238dd3281d045c33489cdc5d516
김민지 <61926751+Min-ji99@users.noreply.github.com>
2022-12-01 22:14:58 +0900

    [level 3] Title: [카카오 인턴] 보석 쇼핑, Time: 78.91 ms, Memory: 17.8 MB -BaekjoonHub

--- 프로그래머스/lv3/67258. ［카카오 인턴］ 보석 쇼핑/［카카오 인턴］ 보석 쇼핑.py
+++ 프로그래머스/lv3/67258. ［카카오 인턴］ 보석 쇼핑/［카카오 인턴］ 보석 쇼핑.py
@@ -1,24 +1,22 @@
 def solution(gems) :
-    answer = [0, len(gems)-1]
-    gems_kind=set(gems)
-    dic={gems[0]:1}
+    answer=[0, len(gems)-1]
+    gem_cnt=len(set(gems))
+    gem_dic={gems[0]:1}
     start, end=0, 0
     while start<len(gems) and end<len(gems) :
-        if len(dic)==len(gems_kind):
-            if(end-start)<(answer[1]-answer[0]) :
+        if len(gem_dic)==gem_cnt :
+            if (end-start)<answer[1]-answer[0] :
                 answer=[start, end]
-            if dic[gems[start]]==1:
-                del dic[gems[start]]
+            if gem_dic[gems[start]]==1 :
+                del gem_dic[gems[start]]
             else:
-                dic[gems[start]]-=1
+                gem_dic[gems[start]]-=1
             start+=1
         else:
             end+=1
             if end==len(gems):
                 break
-            dic[gems[end]]=dic.get(gems[end], 0)+1
-            
+            gem_dic[gems[end]]=gem_dic.get(gems[end], 0)+1
     answer[0]+=1
     answer[1]+=1
-    return answer
     return answer
