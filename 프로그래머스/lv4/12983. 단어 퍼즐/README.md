87394b550f272d3bb3a4eb6c83462da8e2c456dc
김민지 <61926751+Min-ji99@users.noreply.github.com>
2022-12-02 10:31:05 +0900

    [level 4] Title: 단어 퍼즐, Time: 113.97 ms, Memory: 10.5 MB -BaekjoonHub

--- 프로그래머스/lv4/12983. 단어 퍼즐/README.md
+++ 프로그래머스/lv4/12983. 단어 퍼즐/README.md
@@ -1,10 +1,10 @@
 # [level 4] 단어 퍼즐 - 12983 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12983?language=python3) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12983) 
 
 ### 성능 요약
 
-메모리: 10.6 MB, 시간: 106.64 ms
+메모리: 10.5 MB, 시간: 113.97 ms
 
 ### 구분
 
--- 프로그래머스/lv4/12983. 단어 퍼즐/단어 퍼즐.py
+++ 프로그래머스/lv4/12983. 단어 퍼즐/단어 퍼즐.py
@@ -1,14 +1,15 @@
 def solution(strs, t):
     answer = 0
-    n=len(t)
-    dp=[float('inf')]*(n+1)
+    dp=[float('inf')]*(len(t)+1)
     dp[0]=0
-    for i in range(1, n+1):
-        for k in range(1, 6):
-            if i<k : start = 0
-            else : start=i-k
+    for i in range(1, len(t)+1) :
+        for k in range(1, 6) :
+            if i<k :
+                start=0
+            else:
+                start=i-k
             if t[start:i] in strs :
                 dp[i]=min(dp[i], dp[i-k]+1)
-    if dp[-1]==float('inf') :
+    if dp[-1]==float('inf'):
         return -1
-    else : return dp[-1]
+    return dp[-1]
