8db4c8b0863b0c34a0f13cb07ac9a1b583302214
김민지 <61926751+Min-ji99@users.noreply.github.com>
2022-12-07 22:17:30 +0900

    [level 2] Title: 더 맵게, Time: 1923.45 ms, Memory: 71.3 MB -BaekjoonHub

--- 프로그래머스/lv2/42626. 더 맵게/README.md
+++ 프로그래머스/lv2/42626. 더 맵게/README.md
@@ -1,10 +1,10 @@
 # [level 2] 더 맵게 - 42626 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42626) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42626?language=python3) 
 
 ### 성능 요약
 
-메모리: 71.3 MB, 시간: 2171.89 ms
+메모리: 71.3 MB, 시간: 1923.45 ms
 
 ### 구분
 
--- 프로그래머스/lv2/42626. 더 맵게/더 맵게.py
+++ 프로그래머스/lv2/42626. 더 맵게/더 맵게.py
@@ -1,16 +1,15 @@
 import heapq
 
 def solution(scoville, K):
-    answer = 0
     heap=[]
-    for i in scoville:
-        heapq.heappush(heap,i)
-    while heap[0]<K:
-        if len(heap)==1:
+    answer=0
+    for s in scoville :
+        heapq.heappush(heap, s)
+    while heap[0]<K :
+        if len(heap)==1 :
             return -1
-        sco=0
-        sco += heapq.heappop(heap)
-        sco += 2 * heapq.heappop(heap)
-        heapq.heappush(heap, sco)
-        answer += 1
+        a=heapq.heappop(heap)
+        b=heapq.heappop(heap)
+        heapq.heappush(heap, a+b*2)
+        answer+=1
     return answer
