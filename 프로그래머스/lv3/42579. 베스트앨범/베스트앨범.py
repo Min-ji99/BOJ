5f2abf5fb624bd638d768bf784fa41637ea5be1f
김민지 <61926751+Min-ji99@users.noreply.github.com>
2022-12-09 00:00:52 +0900

    [level 3] Title: 베스트앨범, Time: 0.02 ms, Memory: 10 MB -BaekjoonHub

--- 프로그래머스/lv3/42579. 베스트앨범/베스트앨범.py
+++ 프로그래머스/lv3/42579. 베스트앨범/베스트앨범.py
@@ -1,24 +1,22 @@
 from collections import defaultdict
+
 def solution(genres, plays):
-    answer = []
-    genre_dicts=defaultdict(int)
-    genre_arr=[]
+    answer=[]
+    genres_count=defaultdict(int)
+    genres_idx=defaultdict(list)
     for i in range(len(genres)) :
-        genre_dicts[genres[i]]+=plays[i] #총 count
-        genre_arr.append([genres[i], plays[i], i])
-        
-    genre_arr.sort(key=lambda x:(x[0], -x[1], x[2])) #장르별->count->index 순으로 정렬
-    genre_dicts=sorted(genre_dicts.items(), key=lambda x:-x[1]) #총 재생횟수가 큰 순
-    
-    for genre_dict in genre_dicts :
-        #print(genre_dict[0])
+        genres_count[genres[i]]+=plays[i]
+        genres_idx[genres[i]].append([plays[i], i])
+    genres_count=sorted(genres_count, key=lambda x:-genres_count[x])
+    for genre in genres_count :
+        genre_list=genres_idx[genre]
+        genre_list.sort(key=lambda x:(-x[0], x[1]))
+        print(genre_list)
         count=0
-        for genre in genre_arr :
-            if genre_dict[0] == genre[0] :
-                count+=1
-                if count > 2 :
-                    break
-                else :
-                    answer.append(genre[2])
-        
+        for play, idx in genre_list :
+            count+=1
+            if count>2 :
+                break
+            answer.append(idx)
+
     return answer
