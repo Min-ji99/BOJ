b5cba2ce24bee999467040d7d84994a5b4ca931e
김민지 <61926751+Min-ji99@users.noreply.github.com>
2022-12-12 20:21:12 +0900

    [level 3] Title: 순위, Time: 96.13 ms, Memory: 10.8 MB -BaekjoonHub

--- 프로그래머스/lv3/49191. 순위/순위.py
+++ 프로그래머스/lv3/49191. 순위/순위.py
@@ -1,20 +1,19 @@
 def solution(n, results):
-    answer = 0
-    board = [[0]*n for _ in range(n)]
-    
-    for a,b in results:
-        board[a-1][b-1] = 1
-        board[b-1][a-1] = -1
-        
-    for k in range(n):
-        for i in range(n):
-            for j in range(n):
-                if i == j or board[i][j] in [1,-1]:
+    answer=0
+    graph=[[0]*n for _ in range(n)]
+    for i, j in results :
+        graph[i-1][j-1]=1
+        graph[j-1][i-1]=-1
+    for k in range(n) :
+        for i in range(n) :
+            for j in range(n) :
+                if i==j or graph[i][j] in [-1, 1] :
                     continue
-                if board[i][k] == board[k][j] == 1:
-                    board[i][j] = 1
-                    board[j][i] = board[k][i] = board[j][k] = -1
-    for row in board:
-        if row.count(0) == 1:
-            answer += 1
+                if graph[i][k]==graph[k][j]==1 :
+                    graph[i][j]=1
+                    graph[j][i]=graph[j][k]=graph[k][i]=-1
+
+    for row in graph :
+        if row.count(0)==1 :
+            answer+=1
     return answer
