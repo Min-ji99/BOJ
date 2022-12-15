10ff74bac0d483ab667397892dd25b1de327fb00
김민지 <61926751+Min-ji99@users.noreply.github.com>
2022-12-15 15:54:37 +0900

    [level 2] Title: 프린터, Time: 0.09 ms, Memory: 10 MB -BaekjoonHub

--- 프로그래머스/lv2/42587. 프린터/프린터.py
+++ 프로그래머스/lv2/42587. 프린터/프린터.py
@@ -1,23 +1,13 @@
 def solution(priorities, location):
-    answer=1
-    idx=0
-    
-    while(idx<=location) :
-        change=False
-        for i in range(idx+1, len(priorities)) :
-            if(priorities[idx]<priorities[i]) :
-                priorities.append(priorities.pop(idx))
-                if location==idx :
-                    location=len(priorities)-1
-                else:
-                    location-=1
-                change=True
-                break
-        if not change :
-            if idx==location :
-                return answer
-            else:
-                idx+=1
-                answer+=1
+    answer=0
+    queue=[(idx, priority) for idx, priority in enumerate(priorities)]
+    print(queue)
+    while True :
+        cur=queue.pop(0)
+        if any(cur[1]<q[1] for q in queue) :
+            queue.append(cur)
+        else:
+            answer+=1
+            if(cur[0]==location) : return answer
     return answer
                     
