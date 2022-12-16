608f2298f1eb1c637581f508a5d85f48fc9da713
김민지 <61926751+Min-ji99@users.noreply.github.com>
2022-12-16 14:35:04 +0900

    [level 3] Title: 가장 먼 노드, Time: 28.44 ms, Memory: 19.9 MB -BaekjoonHub

--- 프로그래머스/lv3/49189. 가장 먼 노드/가장 먼 노드.py
+++ 프로그래머스/lv3/49189. 가장 먼 노드/가장 먼 노드.py
@@ -1,19 +1,20 @@
 from collections import deque
 def solution(n, edge):
-    q=deque()
     graph=[[] for _ in range(n+1)]
+    q=deque()
     distance=[-1]*(n+1)
+    
     for a, b in edge :
         graph[a].append(b)
         graph[b].append(a)
     q.append(1)
     distance[1]=0
-
+    
     while q :
-        node=q.popleft()
-        for next in graph[node] :
-            if(distance[next]==-1) :
-                distance[next]=distance[node]+1
+        now=q.popleft()
+        for next in graph[now] :
+            if distance[next]==-1:
+                distance[next]=distance[now]+1
                 q.append(next)
     ans=max(distance)
     return distance.count(ans)
