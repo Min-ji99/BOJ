b98894a3e33915b2d160ce972d4a0541f994c296
김민지 <61926751+Min-ji99@users.noreply.github.com>
2023-02-27 20:35:39 +0900

    [level 2] Title: 땅따먹기, Time: 250.72 ms, Memory: 43.1 MB -BaekjoonHub

--- 프로그래머스/lv2/12913. 땅따먹기/땅따먹기.py
+++ 프로그래머스/lv2/12913. 땅따먹기/땅따먹기.py
@@ -1,12 +1,11 @@
-'''
-dp[i][j] : i행 j열을 밟았을 때 최대 점수
-'''
 def solution(land):
     n=len(land)
-    
+    dp=[[0]*4 for _ in range(n)]
+
+    dp[0]=land[0]
+
     for i in range(1, n) :
         for j in range(4) :
-            land[i][j] += max(land[i-1][0:j] + land[i-1][j+1:])
-
-            
-    return max(land[n-1])
+            dp[i][j]=land[i][j]+max(dp[i-1][:j]+dp[i-1][j+1:])
+    
+    return max(dp[n-1])
