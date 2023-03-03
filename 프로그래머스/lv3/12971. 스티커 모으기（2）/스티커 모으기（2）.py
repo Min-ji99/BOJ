a1482a8cb18faed06a8a4ec2266360a5030ce176
김민지 <61926751+Min-ji99@users.noreply.github.com>
2023-03-03 14:43:25 +0900

    [level 3] Title: 스티커 모으기(2), Time: 62.68 ms, Memory: 17.4 MB -BaekjoonHub

--- 프로그래머스/lv3/12971. 스티커 모으기（2）/스티커 모으기（2）.py
+++ 프로그래머스/lv3/12971. 스티커 모으기（2）/스티커 모으기（2）.py
@@ -1,20 +1,15 @@
-'''
-첫번째 행 : 첫번째 스티커를 뜯은 경우
-두번째 행 : 두번째 스티커를 뜯은 경우
-
-현재 위치를 기준으로 두칸 옆과 세칸 옆 스티커 중에 합이 더 커지는 스티커를 뜯어야함
-'''
-def solution(sticker):
+def solution(sticker) :
+    answer=0
     n=len(sticker)
+    
+    if(n==1) : return sticker[0]
+
     dp=[[0]*n for _ in range(2)]
     
-    if n==1:
-        return sticker[0]
-    dp[0][0]=sticker[0]
-    dp[0][1]=sticker[0]
+    dp[0][0], dp[0][1]=sticker[0], sticker[0]
     dp[1][1]=sticker[1]
     
-    for i in range(2, n-1) : #마지막 스티커를 뜯을 수 없음
+    for i in range(2, n-1) :
         dp[0][i]=max(dp[0][i-2]+sticker[i], dp[0][i-1])
         
     for i in range(2, n) :
