aa6159be100a890ab414569121b1129a89d200f2
김민지 <61926751+Min-ji99@users.noreply.github.com>
2023-03-08 15:09:14 +0900

    [level 3] Title: [카카오 인턴] 보석 쇼핑, Time: 65.58 ms, Memory: 17.8 MB -BaekjoonHub

--- 프로그래머스/lv3/67258. ［카카오 인턴］ 보석 쇼핑/［카카오 인턴］ 보석 쇼핑.py
+++ 프로그래머스/lv3/67258. ［카카오 인턴］ 보석 쇼핑/［카카오 인턴］ 보석 쇼핑.py
@@ -1,22 +1,21 @@
-def solution(gems) :
-    answer=[0, len(gems)-1]
-    gem_cnt=len(set(gems))
-    gem_dic={gems[0]:1}
-    start, end=0, 0
-    while start<len(gems) and end<len(gems) :
-        if len(gem_dic)==gem_cnt :
-            if (end-start)<answer[1]-answer[0] :
-                answer=[start, end]
-            if gem_dic[gems[start]]==1 :
-                del gem_dic[gems[start]]
+def solution(gems):
+    answer = [0, len(gems)]
+    gem_kind = len((set(gems)))  # 보석 종류 개수
+    n = len(gems)
+    start, end = 0, 0
+    gem_dict = {gems[0]: 1}
+
+    while start < n and end < n:
+        if len(gem_dict) == gem_kind:
+            if end - start < answer[1] - answer[0]:
+                answer = [start+1, end+1]
             else:
-                gem_dic[gems[start]]-=1
-            start+=1
+                gem_dict[gems[start]] -= 1
+                if gem_dict[gems[start]]==0:
+                    del gem_dict[gems[start]]
+                start += 1
         else:
-            end+=1
-            if end==len(gems):
-                break
-            gem_dic[gems[end]]=gem_dic.get(gems[end], 0)+1
-    answer[0]+=1
-    answer[1]+=1
+            end += 1
+            if end==n : break
+            gem_dict[gems[end]] = gem_dict.get(gems[end], 0)+1
     return answer
