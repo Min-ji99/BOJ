f72861d73ab90b9cf1f3a61187649b1745d9a266
김민지 <61926751+Min-ji99@users.noreply.github.com>
2023-03-08 14:02:25 +0900

    [unrated] Title: 덧칠하기, Time: 1.04 ms, Memory: 11.9 MB -BaekjoonHub

--- 프로그래머스/unrated/161989. 덧칠하기/덧칠하기.py
+++ 프로그래머스/unrated/161989. 덧칠하기/덧칠하기.py
@@ -1,9 +1,8 @@
 def solution(n, m, section):
     answer = 0
-    current=0
+    painted=0
     for s in section :
-        if s>=current :
+        if painted<=s :
+            painted=s+m
             answer+=1
-            current=s+m
-        
     return answer
