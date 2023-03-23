9e6ba0a60f3afe928cf8e03953ece38ebc98a394
김민지 <61926751+Min-ji99@users.noreply.github.com>
2023-03-23 23:37:47 +0900

    [unrated] Title: 롤케이크 자르기, Time: 655.48 ms, Memory: 51.1 MB -BaekjoonHub

--- 프로그래머스/unrated/132265. 롤케이크 자르기/롤케이크 자르기.py
+++ 프로그래머스/unrated/132265. 롤케이크 자르기/롤케이크 자르기.py
@@ -1,21 +1,16 @@
 '''
 동일한 가짓수의 토핑
 '''
+from collections import Counter
+
 def solution(topping):
     answer = 0
     A_dict={}
-    B_dict={}
-    A_dict[topping[0]]=1
+    B_dict=Counter(topping)
     
     if len(topping)==1 : return answer
-
-    for t in topping[1:] :
-        B_dict[t]=B_dict.get(t, 0)+1
-    
-    if len(B_dict)==len(A_dict) :
-            answer+=1
             
-    for i in range(1, len(topping)) :
+    for i in range(len(topping)) :
         if B_dict[topping[i]]>1 :
             B_dict[topping[i]]-=1
         elif B_dict[topping[i]]==1 :
