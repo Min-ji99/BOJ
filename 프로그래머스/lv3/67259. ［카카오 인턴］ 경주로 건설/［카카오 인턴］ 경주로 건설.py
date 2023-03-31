d55bf15151bc1be61d940c01660b0b522301d623
김민지 <61926751+Min-ji99@users.noreply.github.com>
2023-03-31 18:19:17 +0900

    [level 3] Title: [카카오 인턴] 경주로 건설, Time: 0.12 ms, Memory: 10.4 MB -BaekjoonHub

--- 프로그래머스/lv3/67259. ［카카오 인턴］ 경주로 건설/［카카오 인턴］ 경주로 건설.py
+++ 프로그래머스/lv3/67259. ［카카오 인턴］ 경주로 건설/［카카오 인턴］ 경주로 건설.py
@@ -1,33 +1,43 @@
+'''
+cost를 visit 리스트가 아닌 큐에 담아야하는 이유
+먼저 진행되는 방향에 따라 값이 달리질 수도 있다.
+-> 방향 + 비용으로 찾아가야함
+'''
+
 from collections import deque
+
+dx = [0, 0, 1, -1]
+dy = [1, -1, 0, 0]
+
 def solution(board):
-    N = len(board)
-    directions = [(-1,0), (0,-1), (1,0), (0,1)] # 상좌하우
-    
-    def bfs(x,y,cost,d): 
-        graph = [[0]*N for _ in range(N)]
-        for a in range(N):
-            for b in range(N):
-                if board[a][b] == 1 : graph[a][b] = -1  # 벽을 -1로 설정
-        q = deque()
-        q.append((x,y,cost,d))
-        while q:
-            x,y,cost,idx = q.popleft()
-            for i in range(len(directions)):
-                nx = x + directions[i][0]
-                ny = y + directions[i][1]
-                
-                if nx < 0 or nx >= N or ny < 0 or ny >= N : continue
-                if graph[nx][ny] == -1 : continue
+    n = len(board)
 
-                if idx == i : newcost = cost + 100
-                else : newcost = cost + 600
-                
-                if graph[nx][ny] == 0 or ((graph[nx][ny] != 0) and graph[nx][ny] > newcost):
-                    q.append((nx,ny,newcost,i))
-                    graph[nx][ny] = newcost
-                    
-                else : continue
+    def bfs(x, y, cost, dir):
+        visit = [[int(1e9)] * n for _ in range(n)]
+        visit[0][0] = 0
 
-        return graph[N-1][N-1]
+        queue = deque()
+        queue.append((x, y, cost, dir))  # (시작X, 시작Y, 시작Cost, 시작방향)
 
-    return min(bfs(0,0,0,2),bfs(0,0,0,3))
+        while queue:
+            x, y, cost, dir = queue.popleft()
+
+            if x == n - 1 and y == n - 1:
+                continue
+
+            for i in range(4):
+                nx = x + dx[i]
+                ny = y + dy[i]
+                if 0<=nx<n and 0<=ny<n and board[nx][ny]==0:
+                    if dir==i:
+                        nc = cost + 100
+                    else:
+                        nc = cost + 600
+
+                    if nc <= visit[nx][ny]:
+                        visit[nx][ny] = nc
+                        queue.append((nx, ny, nc, i))
+
+        return visit[-1][-1]
+    return min(bfs(0, 0, 0, 0), bfs(0, 0, 0, 2))
+
