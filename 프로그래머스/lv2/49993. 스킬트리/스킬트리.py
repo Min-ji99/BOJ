4aad8e15d29657526339c0fc88c3e94115d91959
김민지 <61926751+Min-ji99@users.noreply.github.com>
2023-04-10 20:26:14 +0900

    [level 2] Title: 스킬트리, Time: 0.01 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/lv2/49993. 스킬트리/스킬트리.py
+++ 프로그래머스/lv2/49993. 스킬트리/스킬트리.py
@@ -1,10 +1,16 @@
 def solution(skill, skill_trees):
     answer = 0
+    
     for skill_tree in skill_trees :
-        s=""
-        for sk in skill_tree :
-            if sk in skill:
-                s+=sk
-        if skill[:len(s)]==s :
+        pos=0
+        flag=True
+        for i in range(len(skill_tree)) :
+            if skill_tree[i] in skill : #해당 스킬이 선행 스킬 순서에 포함되어있는지 확인
+                if skill_tree[i] != skill[pos] :
+                    flag=False
+                    break
+                else:
+                    pos+=1
+        if flag :
             answer+=1
     return answer
