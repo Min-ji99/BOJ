ca2ccfaab641974cf3e5962b0f379fd58b7f98b8
김민지 <61926751+Min-ji99@users.noreply.github.com>
2023-06-01 22:23:50 +0900

    [level 3] Title: 기둥과 보 설치, Time: 1745.51 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/lv3/60061. 기둥과 보 설치/기둥과 보 설치.py
+++ 프로그래머스/lv3/60061. 기둥과 보 설치/기둥과 보 설치.py
@@ -1,30 +1,29 @@
-def check(answer):
-    for x, y, stuff in answer:
-        if stuff == 0: #기둥 체크
-            #'바닥 위' or '보의 한쪽 끝 위' or '또 다른 기둥 위' 
-            if y == 0 or [x-1, y, 1] in answer or [x, y, 1] in answer or [x, y-1, 0] in answer:
+def check(answer) :
+    for x, y, a in answer:
+        if a==0 : #기둥
+            if y==0 or [x, y-1, 0] in answer or [x-1, y, 1] in answer or [x, y, 1] in answer:
                 continue
             return False
-        elif stuff == 1: #보 체크
-            #'한쪽 끝 부분이 기둥 위' or '양쪽 끝 부분이 다른 보와 동시 연결'
-            if [x, y-1, 0] in answer or [x+1, y-1, 0] in answer or ([x-1, y, 1] in answer and [x+1, y, 1] in answer):
+        else: #보
+            if [x+1, y-1, 0] in answer or [x, y-1, 0] in answer or ([x-1, y, 1] in answer and [x+1, y, 1] in answer) :
                 continue
             return False
     return True
 
-def solution(n, build_frame):
-    answer = []
-    
+def solution(n, build_frame) :
+    answer=[]
+
     for frame in build_frame:
         x, y, a, b=frame
-        
+
         if b==0 : #삭제
             answer.remove([x, y, a])
             if not check(answer) :
                 answer.append([x, y, a])
-        elif b==1 : #설치
+        else:
             answer.append([x, y, a])
             if not check(answer) :
                 answer.remove([x, y, a])
+
     answer.sort()
     return answer
