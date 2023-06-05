3383f06db6b16642b91df698e2a84d039157ec9b
김민지 <61926751+Min-ji99@users.noreply.github.com>
2023-06-05 20:31:14 +0900

    [level 3] Title: [카카오 인턴] 보석 쇼핑, Time: 69.24 ms, Memory: 17.9 MB -BaekjoonHub

--- 프로그래머스/lv3/67258. ［카카오 인턴］ 보석 쇼핑/［카카오 인턴］ 보석 쇼핑.py
+++ 프로그래머스/lv3/67258. ［카카오 인턴］ 보석 쇼핑/［카카오 인턴］ 보석 쇼핑.py
@@ -1,21 +1,23 @@
 def solution(gems):
-    answer = [0, len(gems)]
-    gem_kind = len((set(gems)))  # 보석 종류 개수
-    n = len(gems)
-    start, end = 0, 0
-    gem_dict = {gems[0]: 1}
-
-    while start < n and end < n:
-        if len(gem_dict) == gem_kind:
-            if end - start < answer[1] - answer[0]:
-                answer = [start+1, end+1]
+    answer=[1, len(gems)]
+        
+    kinds=len(set(gems)) #보석 종류 개수
+    n=len(gems)
+    start, end=0, 0
+    windows={gems[0]:1} # 구간 내의 보석 종류 count
+    
+    while start<n and end<n :
+        if len(windows)== kinds:
+            if end-start<answer[1]-answer[0]:
+                answer=[start+1, end+1]
             else:
-                gem_dict[gems[start]] -= 1
-                if gem_dict[gems[start]]==0:
-                    del gem_dict[gems[start]]
-                start += 1
+                windows[gems[start]]-=1
+                if windows[gems[start]]==0 :
+                    del windows[gems[start]]
+                start+=1
         else:
-            end += 1
-            if end==n : break
-            gem_dict[gems[end]] = gem_dict.get(gems[end], 0)+1
+            end+=1
+            if end==n:
+                break
+            windows[gems[end]]=windows.get(gems[end], 0)+1
     return answer
